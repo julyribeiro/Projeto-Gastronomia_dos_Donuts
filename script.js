29308d3fd1b73b0b37bdc5905d1420c27aedf2a7
julyribeiro <july.026@hotmail.com>
2024-07-07 16:40:47 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+window.onload = function() {
+    alert("Bem-vindo ao melhor evento de Gastronomia dos Donuts! Aqui você vai aprender muito e se deliciar com os melhores donuts do mundo. Clique em OK para explorar nossas atrações e se preparar para um mundo de sabores!");
+};
